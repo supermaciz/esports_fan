b50d1fa1970397a8bbf66d9b8629e4c8c7debdff
Mathieu Cizo <maciz@outlook.fr>
2025-12-03 18:03:58 +0100

    feat: user authentication
    - Implemented tests for user session management in `UserSessionControllerTest`.
    - Created tests for user confirmation and login via magic link in `UserLive.ConfirmationTest`.
    - Added tests for login functionality and navigation in `UserLive.LoginTest`.
    - Developed registration tests in `UserLive.RegistrationTest`.
    - Implemented user settings tests in `UserLive.SettingsTest`.
    - Added comprehensive user authentication tests in `UserAuthTest`.
    - Introduced helper functions in `ConnCase` for user registration and login.
    - Created `AccountsFixtures` for generating user data and tokens.

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :esports_fan, :scopes,
+  user: [
+    default: true,
+    module: EsportsFan.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: EsportsFan.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :esports_fan,
   ecto_repos: [EsportsFan.Repo],
   generators: [timestamp_type: :utc_datetime]
